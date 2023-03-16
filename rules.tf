6365d7e231e933391aaa8b6dd8ed2bfcb9fde2b3
vitali-federau-fivestars <vitali.federau@fivestars.com>
2023-03-16 03:40:29 +0300

    [Captcha support] Adding rule condition

--- rules.tf
+++ rules.tf
@@ -246,6 +246,10 @@ resource "aws_wafv2_web_acl" "default" {
           for_each = rule.value.action == "count" ? [1] : []
           content {}
         }
+        dynamic "captcha" {
+          for_each = rule.value.action == "captcha" ? [1] : []
+          content {}
+        }
       }
 
       statement {
