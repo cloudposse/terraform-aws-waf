8edf761b821855b5910e4e227716997b1a2b8163
Kévin Hoste <kevinhoste@proton.me>
2023-11-24 18:34:08 +0100

    fix content_type in aws_wafv2_web_acl to use correct map value

--- rules.tf
+++ rules.tf
@@ -129,7 +129,7 @@ resource "aws_wafv2_web_acl" "default" {
     content {
       key          = custom_response_body.key
       content      = custom_response_body.value.content
-      content_type = custom_response_body.value.content
+      content_type = custom_response_body.value.content_type
     }
   }
 
