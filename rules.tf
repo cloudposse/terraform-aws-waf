c9826d7aef158adcfdea2cb1ae8f627c0d80f93d
Guus De Graeve <guus@hauws.com>
2024-05-21 10:21:28 +0200

    fix scope_down_statement

--- rules.tf
+++ rules.tf
@@ -716,9 +716,9 @@ resource "aws_wafv2_web_acl" "default" {
           for_each = rule.value.action == "block" ? [1] : []
           content {
             dynamic "custom_response" {
-              for_each = rule.value.action == "block" && rule.value.custom_response_code != null ? [1] : []
+              for_each = rule.value.custom_response_code != null ? [rule.value.custom_response_code] : []
               content {
-                response_code = rule.value.custom_response_code
+                response_code = custom_response.value
               }
             }
           }
@@ -747,36 +747,44 @@ resource "aws_wafv2_web_acl" "default" {
 
               content {
                 dynamic "and_statement" {
-                  for_each = lookup(scope_down_statement.value, "and", null) != null ? scope_down_statement.value.and : []
+                  for_each = length(lookup(scope_down_statement.value, "and", [])) > 0 ? [scope_down_statement.value.and] : []
                   content {
-                    statement {
-                      dynamic "byte_match_statement" {
-                        for_each = lookup(and_statement.value, "byte_match_statement", null) != null ? [and_statement.value.byte_match_statement] : []
-                        content {
-                          search_string         = byte_match_statement.value.search_string
-                          positional_constraint = byte_match_statement.value.positional_constraint
-                          field_to_match {
-                            dynamic "uri_path" {
-                              for_each = lookup(byte_match_statement.value, "uri_path", null) != null ? [1] : []
-                              content {}
-                            }
-                            dynamic "single_header" {
-                              for_each = lookup(byte_match_statement.value, "single_header", null) != null ? [1] : []
-                              content {
-                                name = single_header.value
+                    dynamic "statement" {
+                      for_each = and_statement.value
+                      content {
+                        dynamic "byte_match_statement" {
+                          for_each = lookup(statement.value, "byte_match_statement", null) != null ? [statement.value.byte_match_statement] : []
+                          content {
+                            search_string         = byte_match_statement.value.search_string
+                            positional_constraint = byte_match_statement.value.positional_constraint
+                            field_to_match {
+                              dynamic "uri_path" {
+                                for_each = lookup(byte_match_statement.value, "uri_path", null) != null ? [1] : []
+                                content {}
+                              }
+                              dynamic "single_header" {
+                                for_each = lookup(byte_match_statement.value, "single_header", null) != null ? [byte_match_statement.value.single_header] : []
+                                content {
+                                  name = single_header.value
+                                }
+                              }
+                              dynamic "method" {
+                                for_each = lookup(byte_match_statement.value, "method", null) != null ? [1] : []
+                                content {}
                               }
                             }
-                            dynamic "method" {
-                              for_each = lookup(byte_match_statement.value, "method", null) != null ? [1] : []
-                              content {}
-                            }
-                          }
-                          dynamic "text_transformation" {
-                            for_each = lookup(byte_match_statement.value, "text_transformation", null) != null ? byte_match_statement.value.text_transformation : []
+                            dynamic "text_transformation" {
+                              for_each = lookup(byte_match_statement.value, "text_transformation", [
+                                {
+                                  priority = 0
+                                  type     = "NONE"
+                                }
+                              ])
 
-                            content {
-                              priority = text_transformation.value.priority
-                              type     = text_transformation.value.type
+                              content {
+                                priority = text_transformation.value.priority
+                                type     = text_transformation.value.type
+                              }
                             }
                           }
                         }
