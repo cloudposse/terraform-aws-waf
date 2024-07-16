4ac3ba594bb176f9e723b6cf1419aebef3df492b
RoseSecurity <michaeljrosenfeld22@gmail.com>
2024-07-15 22:39:02 -0500

    fix: fix scope down statement text transformations

--- rules.tf
+++ rules.tf
@@ -811,7 +811,7 @@ resource "aws_wafv2_web_acl" "default" {
 
                     dynamic "text_transformation" {
                       for_each = lookup(byte_match_statement.value, "text_transformation", null) != null ? [
-                        for rule in rule.value.statement.text_transformation : {
+                        for rule in byte_match_statement.value.text_transformation : {
                           priority = rule.priority
                           type     = rule.type
                       }] : []
