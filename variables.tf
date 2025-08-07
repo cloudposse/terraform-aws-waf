07e51b2f62630427980d2b4fd1b795c37ee7b164
RoseSecurity <michael@rosesecurity.dev>
2025-08-07 12:11:46 -0400

    feat: add not statement for bot control rules

--- variables.tf
+++ variables.tf
@@ -370,9 +370,29 @@ variable "managed_rule_group_statement_rules" {
     }), null)
     rule_label = optional(list(string), null)
     statement = object({
-      name        = string
-      vendor_name = string
-      version     = optional(string)
+      name                             = string
+      vendor_name                      = string
+      use_not_statement_for_scope_down = optional(bool, false)
+      scope_down_statement = optional(object({
+        byte_match_statement = object({
+          positional_constraint = string
+          search_string         = string
+          field_to_match = object({
+            all_query_arguments   = optional(bool)
+            body                  = optional(bool)
+            method                = optional(bool)
+            query_string          = optional(bool)
+            single_header         = optional(object({ name = string }))
+            single_query_argument = optional(object({ name = string }))
+            uri_path              = optional(bool)
+          })
+          text_transformation = list(object({
+            priority = number
+            type     = string
+          }))
+        })
+      }), null)
+      version = optional(string)
       rule_action_override = optional(map(object({
         action = string
         custom_request_handling = optional(object({
@@ -429,25 +449,6 @@ variable "managed_rule_group_statement_rules" {
           }), null)
         }), null)
       })), null)
-      scope_down_statement = optional(object({
-        byte_match_statement = object({
-          positional_constraint = string
-          search_string         = string
-          field_to_match = object({
-            all_query_arguments   = optional(bool)
-            body                  = optional(bool)
-            method                = optional(bool)
-            query_string          = optional(bool)
-            single_header         = optional(object({ name = string }))
-            single_query_argument = optional(object({ name = string }))
-            uri_path              = optional(bool)
-          })
-          text_transformation = list(object({
-            priority = number
-            type     = string
-          }))
-        })
-      }), null)
     })
     visibility_config = optional(object({
       cloudwatch_metrics_enabled = optional(bool)
@@ -599,7 +600,7 @@ variable "rate_based_statement_rules" {
         field_to_match:
           Part of a web request that you want AWS WAF to inspect.
         positional_constraint:
-          Area within the portion of a web request that you want AWS WAF to search for search_string. 
+          Area within the portion of a web request that you want AWS WAF to search for search_string.
           Valid values include the following: `EXACTLY`, `STARTS_WITH`, `ENDS_WITH`, `CONTAINS`, `CONTAINS_WORD`.
         search_string:
           String value that you want AWS WAF to search for.
